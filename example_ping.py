6a26acd510fd5f7351803d531dd0207cb2180022
Scott Greenwald <swgreen@mit.edu>
2014-05-20 18:12:23 -0400

    Does periodic send/receive

--- example_ping.py
+++ example_ping.py
@@ -8,6 +8,11 @@ def ws_parse(parser):
     print "running ws_parse"
     wearscript.parse(callback, parser)
 
+def periodic_send(ws):
+	#TODO(scottgwald): catch dead websocket
+    ws.send('blob', 'theMessage34234', 'fromOneToTheOther')
+    gevent.spawn_later(5, periodic_send, ws)
+
 def callback(ws, **kw):
 
     def get_ping(chan, resultChan, timestamp):
@@ -18,9 +23,14 @@ def callback(ws, **kw):
     def get_pong(chan, timestamp):
     	print "Got pong %5f" % timestamp
 
+    def get_blob(chan, title, body):
+    	print "Got blob %s %s" % (title,body)
+
     ws.subscribe('ping', get_ping)
     ws.subscribe('pong', get_pong)
+    ws.subscribe('blob', get_blob)
     ws.send('ping', 'pong', time.time())
+    gevent.spawn_later(5, periodic_send, ws)
     ws.handler_loop()
 
 if __name__ == '__main__':
