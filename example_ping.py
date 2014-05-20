784cb3010bf183fa659ad83ebb173d5c8fa7efe0
Scott Greenwald <swgreen@mit.edu>
2014-05-20 18:01:40 -0400

    fixed example
    publish doesn't work correctly without the Go server,
    so use "send" directly instead

--- example_ping.py
+++ example_ping.py
@@ -6,11 +6,16 @@ import time
 def callback(ws, **kw):
 
     def get_ping(chan, resultChan, timestamp):
-    	print "Got ping %s" % timestamp
-        ws.publish(resultChan, timestamp, time.time(), ws.group_device)
+    	print "Got ping %5f" % timestamp
+    	ws.send(resultChan, time.time());
+        # ws.publish(resultChan, timestamp, time.time(), ws.group_device)
+
+    def get_pong(chan, timestamp):
+    	print "Got pong %5f" % timestamp
 
     ws.subscribe('ping', get_ping)
-    ws.send('ping', 'url', 'fromServerToClientLater')
+    ws.subscribe('pong', get_pong)
+    ws.send('ping', 'pong', time.time())
     ws.handler_loop()
 
 wearscript.parse(callback, argparse.ArgumentParser())
