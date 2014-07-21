6de43df0802bc459dce6d08ce5d4c4c3824ab469
Scott Greenwald <swgreen@mit.edu>
2014-07-21 14:42:47 -0400

    switch glass script module to one owned by my primary github acct
    also update setup-glass.sh accordingly

--- setup-glass.sh
+++ setup-glass.sh
@@ -5,7 +5,7 @@ adb shell rm -r /sdcard/wearscript/gists/*
 
 # install this script
 adb shell mkdir /sdcard/wearscript/gists/glassprov
-adb push b612c849601a8de2ecae /sdcard/wearscript/gists/glassprov
+adb push 4fd78440330c45376aa0 /sdcard/wearscript/gists/glassprov
 
 # set endpoint
 # setendpoint.sh
